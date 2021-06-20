473fb9d44ccca0a3b8166e903f1eae69144b73ee
Soham Patel <sohampatel1306@gmail.com>
2021-06-20 16:32:12 -0500

    Created schema for creating a user

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const router = express.Router();
-
+const { check, validationResult } = require("express-validator");
+const User = require("../models/Users");
 // @route POST api/user
 // @desc  Register a user
 // @access Public
-router.post("/", (req, res) => {
-  res.send("Register a user");
-});
+router.post(
+  "/",
+  [
+    check("name", "Please add name").not().isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with length more than 6"
+    ).isLength({ min: 6, max: 20 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errros: errors.array() });
+    }
+    res.send('passed')
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 // connect DB
 connectDB();
 
+// Init middleware
+app.use(express.json({extended: false}))
+
 app.get("/", (req, res) =>
   res.json({ msg: "Welcome to the Finance application!" })
 );
